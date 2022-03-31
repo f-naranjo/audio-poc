4bc13017b89422bf6c3c600f64f3c3fe0b2c2fe3
Fran Naranjo <frannaranjo@MBPdeFrancisco.lan>
2022-03-31 10:01:08 +0100

    add regions

--- README.md
+++ README.md
@@ -6,42 +6,22 @@ Key Features:
 - [ ] Audio upload from local files
 - [ ] Audio trim
 - [x] Concat audios
+- [x] Create audio clips (regions)
 - [ ] Change audio parts order
-- [ ] Preview current audio state
+- [x] Preview current audio state
 - [ ] Change playback speed
 - [ ] Audio recording
 
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Caveats:
 
-## Getting Started
+- Regions disappears when new audio is recorded (It may be related to state handling and Waveform component re render)
 
-First, run the development server:
+Roadmap:
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+- [ ] Normalize audio formats when user uploads files
+- [ ] Handle audio trim with backend
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Links / References:
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [https://tabsoverspaces.in/posts/create-a-audio-player-in-nextjs/](https://tabsoverspaces.in/posts/create-a-audio-player-in-nextjs/)
+- [https://ffmpeg.org/](https://ffmpeg.org/)
--- components/WaveForm/index.js
+++ components/WaveForm/index.js
@@ -1,8 +1,10 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import Wavesurfer from 'wavesurfer.js';
+import * as WaveformRegionsPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions';
 
 const Waveform = ({ url, blob, name, ...props }) => {
   const waveform = useRef(null);
+  const [activeRegion, setActiveRegion] = useState(null);
 
   useEffect(() => {
     // Check if wavesurfer object is already created.
@@ -17,6 +19,7 @@ const Waveform = ({ url, blob, name, ...props }) => {
         barRadius: 3,
         cursorWidth: 3,
         cursorColor: '#567FFF',
+        plugins: [WaveformRegionsPlugin.create({ maxLength: 90 })],
       });
       // Load audio from a remote url.
       /* To load a local audio file
@@ -32,8 +35,37 @@ const Waveform = ({ url, blob, name, ...props }) => {
         waveform.current.load(url);
         return;
       }
+      // Enable dragging on the audio waveform
     }
-  }, [blob]);
+    waveform.current.enableDragSelection({
+      maxLength: 90,
+    });
+    // Perform action when new region is created
+    waveform.current.on('region-created', (e) => {
+      let color = 'rgba(0,0,0,0.3)';
+      e.color = color;
+      console.log(e);
+      setActiveRegion(e);
+    });
+
+    waveform.current.on('region-click', (region) => {
+      let color = 'rgba(0,0,0,0.5)';
+      region.color = color;
+      console.log(region);
+      setActiveRegion(region);
+    });
+  }, [blob, name, url]);
+
+  // delete a particular region
+  const deleteClip = () => {
+    waveform.current.regions.list[activeRegion.id].remove();
+    setActiveRegion(null);
+  };
+
+  // play a particular region
+  const playClip = () => {
+    waveform.current.regions.list[activeRegion.id].play();
+  };
 
   const playAudio = () => {
     // Check if the audio is already playing
@@ -51,13 +83,23 @@ const Waveform = ({ url, blob, name, ...props }) => {
     >
       <div id={name} />
       <div
-        flexDirection="row"
-        justifyContent="center"
+        flexdirection="row"
+        justifycontent="center"
         style={{ display: 'flex', justify: 'center' }}
       >
         <button style={{ margin: '4px' }} onClick={playAudio}>
           Play / Pause
         </button>
+        {activeRegion && (
+          <>
+            <button style={{ margin: '4px' }} onClick={playClip}>
+              Play clip
+            </button>
+            <button style={{ margin: '4px' }} onClick={deleteClip}>
+              Delete clip
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
